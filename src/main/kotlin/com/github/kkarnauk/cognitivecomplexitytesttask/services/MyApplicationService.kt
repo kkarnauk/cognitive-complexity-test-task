c2eaadf18edf36f50e6f011853e6c7e4c97e9c4f
GitHub Action <action@github.com>
2021-09-04 10:12:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kkarnauk/cognitivecomplexitytesttask/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kkarnauk.cognitivecomplexitytesttask.services
+
+import com.github.kkarnauk.cognitivecomplexitytesttask.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kkarnauk/cognitivecomplexitytesttask/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kkarnauk/cognitivecomplexitytesttask/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kkarnauk.cognitivecomplexitytesttask.services
 
+import com.github.kkarnauk.cognitivecomplexitytesttask.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
