b9576d2c50e7d1cb535ea29f1bb4b1e8be0780bd
Kirill <bitikit@gmail.com>
2021-09-04 13:42:26 +0300

    Cleanup project

--- src/main/kotlin/org/jetbrains/cognitivecomplexity/bundle/Bundle.kt
+++ src/main/kotlin/org/jetbrains/cognitivecomplexity/bundle/Bundle.kt
@@ -1,13 +1,13 @@
-package com.github.kkarnauk.cognitivecomplexitytesttask
+package org.jetbrains.cognitivecomplexity.bundle
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.Bundle"
 
-object MyBundle : AbstractBundle(BUNDLE) {
+object Bundle : AbstractBundle(BUNDLE) {
 
     @Suppress("SpreadOperator")
     @JvmStatic
