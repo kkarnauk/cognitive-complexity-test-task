e194b1bb7a93519c4213a8a0112edc1f68dbffea
Kirill <bitikit@gmail.com>
2021-09-04 16:21:28 +0300

    Add action: type of current file

---
+++ src/main/kotlin/org/jetbrains/cognitivecomplexity/action/CurrentFileTypeAction.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.cognitivecomplexity.action
+
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import org.jetbrains.cognitivecomplexity.bundle.Bundle
+
+class CurrentFileTypeAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val fileTypeName = e.dataContext.getData(CommonDataKeys.VIRTUAL_FILE)?.fileType?.displayName
+            ?: Bundle.message("noCurrentFileError")
+        notificationGroup.createNotification(fileTypeName, NotificationType.INFORMATION).notify(e.project)
+    }
+
+    companion object {
+        private val notificationGroup = NotificationGroup.balloonGroup("cognitive.complexity")
+    }
+}
